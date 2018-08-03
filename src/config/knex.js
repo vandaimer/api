486413901b96f7fd73c6838613ad532ac2a21a7e
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-03 20:31:11 -0300

    Implement runMigrations func on config.knex

--- src/config/knex.js
+++ src/config/knex.js
@@ -3,5 +3,13 @@ import config from './knexConfig';
 
 const connection = knex(config);
 
+export async function runMigrations() {
+  try {
+    await connection.migrate.latest();
+  } catch (error) {
+    connection.destroy();
+    throw error;
+  }
+}
 
 export default connection;
