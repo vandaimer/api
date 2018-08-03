57f5bdd61c5ca525eb3c8cbb80f4ef6870781b95
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-03 20:38:23 -0300

    Expose person routes

--- src/core/index.js
+++ src/core/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import contacts from './contacts';
+import person from './person';
 
 const router = Router();
 
 router.use('/contacts', contacts);
+router.use('/person', person);
 
 export default router;
