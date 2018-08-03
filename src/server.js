c339ad332db87638a367a014b1dbd581490f7466
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-03 20:32:41 -0300

    Run all migrations befero run express

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import api from './express/api';
+import { runMigrations } from './config/knex';
 
 const port = process.env.PORT || 3000;
 
-api(port)
- .then(() => {
-  // eslint-disable-next-line no-console
-  console.info(`Server running. See http://localhost:${port}`);
-})
- .catch(err =>
-  // eslint-disable-next-line no-console
-  console.error('Unable to start the api:', err),
-);
+runMigrations()
+  .then(() => api(port))
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.info(`Server running. See http://localhost:${port}`);
+  })
+  .catch(err =>
+    // eslint-disable-next-line no-console
+    console.error('Unable to start the api:', err),
+  );
