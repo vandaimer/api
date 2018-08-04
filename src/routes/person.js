d0e1b3871a2c3a4bea045103ea4a5f5cf5039508
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-04 02:11:35 -0300

    Implement response handle on routes.person

--- src/routes/person.js
+++ src/routes/person.js
@@ -3,8 +3,32 @@ import { Router } from 'express';
 import Person from '../core/Person';
 
 const router = Router();
-router.delete('/:id([0-9]+)', Person.remove);
-router.get('/', Person.list);
-router.post('/', Person.create);
+
+router.delete('/:id([0-9]+)', async (req, res) => {
+  try {
+    await Person.remove(req);
+    return res.status(204).json();
+  } catch ({ message }) {
+    return res.status(400).json({ message });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const created = await Person.create(req);
+    return res.status(200).json(created);
+  } catch ({ message }) {
+    return res.status(400).json({ message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const items = await Person.list();
+    return res.status(200).json({ items });
+  } catch (error) {
+    return res.status(400).send();
+  }
+});
 
 export default router;
