8b0a1b4cc4af97e8a7617108b24d5509fd30b4fc
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-03 21:23:12 -0300

    Remove contacts module

--- src/core/contacts/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import list from './list';
-
-const router = Router();
-
-router.get('/', list);
-
-export default router;
--- src/core/index.js
+++ src/core/index.js
@@ -1,11 +1,9 @@
 import { Router } from 'express';
 
-import contacts from './contacts';
 import person from './person';
 
 const router = Router();
 
-router.use('/contacts', contacts);
 router.use('/person', person);
 
 export default router;
