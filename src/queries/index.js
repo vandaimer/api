b3da5965ccaa497616ca54075914bcff9226f19b
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-03 21:27:39 -0300

    Export createBatch func on queries module

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,9 +1,19 @@
 import create from './create';
+import createBatch from './createBatch';
 import list from './list';
 import queryBuilder from '../config/knex';
 import remove from './remove';
 import update from './update';
 
-const { transaction } = queryBuilder;
+const {
+  transaction
+} = queryBuilder;
 
-export { create, list, remove, transaction, update };
+export {
+  create,
+  createBatch,
+  list,
+  remove,
+  transaction,
+  update
+};
