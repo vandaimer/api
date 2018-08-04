49d37a9979c1e1ec6b507361d5ab15aad1520f34
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-03 21:27:18 -0300

    Improve the list from person module to retrive the contact list

--- src/core/person/list.js
+++ src/core/person/list.js
@@ -1,6 +1,15 @@
 import { list } from '../../queries';
 
 export default async function (req, res) {
-  const persons = await list('person', {});
+  let persons = await list('person', {});
+
+  persons = persons.map(async person => {
+    const { id } = person;
+    const contacts = await list('contact', { personId: id });
+    return { ...person, contacts };
+  });
+
+  persons = await Promise.all(persons);
+
   return res.json({ persons });
 };
