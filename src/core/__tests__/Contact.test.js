3f418e89a8176c0777eedb3aafd48e1d70c5a0e6
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-04 22:25:12 -0300

    Add updateByPersonId tests on core.Contact.test

--- src/core/__tests__/Contact.test.js
+++ src/core/__tests__/Contact.test.js
@@ -4,27 +4,40 @@ import { list, createBatch, removeWhere } from '../../queries';
 jest.mock('../../queries');
 
 describe('core.Contact', () => {
+  const personId = 1;
+  const contact = { data: 'mocked' };
+  const contacts = [contact];
+
   it('should call listByPersonId', async () => {
-    const personId = 1;
     await Contact.listByPersonId(personId);
 
     expect(list).toBeCalledWith(Contact.tableName, { personId });
   });
 
   it('should call saveBatch', async () => {
-    const personId = 1;
-    const contact = { data: 'mocked' };
-    const contacts = [ contact ];
     await Contact.saveBatch(personId, contacts);
-    const expected = [ { ...contact, personId } ];
+    const expected = [{ ...contact, personId }];
 
     expect(createBatch).toBeCalledWith(Contact.tableName, expected);
   });
 
   it('should call removeByPersonId', async () => {
-    const personId = 1;
     await Contact.removeByPersonId(personId);
 
     expect(removeWhere).toBeCalledWith(Contact.tableName, { personId });
+  });
+
+  it('should call saveBatch when call updateByPersonId', async () => {
+    Contact.saveBatch = jest.fn();
+    await Contact.updateByPersonId(personId, contacts);
+
+    expect(Contact.saveBatch).toBeCalledWith(personId, contacts);
+  });
+
+  it('should call removeByPersonId when call updateByPersonId', async () => {
+    Contact.removeByPersonId = jest.fn();
+    await Contact.updateByPersonId(personId, contacts);
+
+    expect(Contact.removeByPersonId).toBeCalledWith(personId);
   });
 });
